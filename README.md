4aec31afa8835d66ef38784e0a2d5d4d2b9b9138
zannatul-nayeem <zannatul.nayeem1000@gmail.com>
2020-09-06 17:11:14 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# coffeat
+# coffeat-master
---
+++ js/custom.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+    //slider_testimonial   
+    $('.Testimonial-part-sllider-wrap').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        arrows: false,
+        dots: true,
+        fade: true,
+    });
+
+});
